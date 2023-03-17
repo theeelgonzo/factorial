08c79b2997fa935b51a93bc3e03236602210782c
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-16 20:02:00 -0500

    Remove for loop to correct conditional warning when non-number value is entered as arg.

--- main.js
+++ main.js
@@ -2,18 +2,14 @@
 console.log(num); */
 
 function factorial(number) {
-  for (let i = number; i >= 0; i--) {
-    if (typeof i !== "number") {
-      console.log("Write a number....or else, yark-o!");
-    } else if (number == 0) {
-      return 1;
-    } else if (number < 0) {
-      return NaN;
-    } else if (typeof number == Number) {
-      return NaN;
-    } else {
-      return number * factorial(number - 1);
-    }
+  if (typeof number !== "number") {
+    console.log("Write a number....or else, yark-o!");
+  } else if (number == 0) {
+    return 1;
+  } else if (number < 0) {
+    return NaN;
+  } else {
+    return number * factorial(number - 1);
   }
 }
 
