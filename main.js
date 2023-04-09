a8debfadcb9641549a4b6f5ef5bb968cccf67d33
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-08 21:15:34 -0500

    Cheap

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 console.log(num); */
 
 function factorial(number) {
-  if (typeof number !== "number") {
-    console.log("Write a number....or else, yark-o!");
-  } else if (number == 0) {
-    return 1;
-  } else if (number < 0) {
-    return NaN;
-  } else {
-    return number * factorial(number - 1);
-  }
+    if (typeof number !== 'number') {
+        console.log('Write a number....or else, yark-o!');
+    } else if (number == 0) {
+        return 1;
+    } else if (number < 0) {
+        return NaN;
+    } else {
+        return number * factorial(number - 1);
+    }
 }
 
 console.log(factorial(0));
 console.log(factorial(4));
 console.log(factorial(10));
-console.log(factorial("potato"));
+console.log(factorial('potato'));
 console.log(factorial(-2));
+console.log('This is a cheap commit, and I know it.');
