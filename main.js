d8a5860db31fd43aaff595139f5b8f71949538e7
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-29 18:26:27 -0500

    Add output function. Broken.

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 /* const num = 1;
 console.log(num); */
 
+const theMaw = document.getElementById('maw');
+const subBut = document.getElementById('subBut');
+const boxOut = document.getElementById('boxOut');
+
+function getValue() {
+    let value = theMaw.value;
+    console.log(`Your value is ${value}`);
+    return value;
+}
+
 function factorial(number) {
     if (typeof number !== 'number') {
         console.log('Write a number....or else, yark-o!');
     } else if (number == 0) {
-        return 1;
+        let result = 1;
+        return result;
     } else if (number < 0) {
         return NaN;
     } else {
-        return number * factorial(number - 1);
+        let result = number * factorial(number - 1);
+        return result;
     }
 }
+
+function boxPrint(result) {
+    boxOut.textContent = result;
+}
+
+function solveFact() {
+    boxPrint(factorial(getValue()));
+}
+
+subBut.addEventListener('click', solveFact);
 
 console.log(factorial(0));
 console.log(factorial(4));
